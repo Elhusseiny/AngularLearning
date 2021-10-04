350ced7336bfc61c146d10a86a310417c11a2c9e
Mohamed Elhussein <mohamed.elhussein@arpuplus.com>
2021-10-05 00:39:43 +0200

    1- @input decoration. 2- #reference with @viewChild 3- ngcontent to add content between component used in another component. 4- event binding. 5- static checking ! 6- use of objects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildElementComponent } from './child-element/child-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildElementComponent
   ],
   imports: [
     BrowserModule,
