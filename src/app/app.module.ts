d6e791a8fa11e5ba6ee97323c9405f887d67795e
Mohamed Elhussein <mohamed.elhussein@arpuplus.com>
2021-10-06 04:01:33 +0200

    custom directive 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChildElementComponent } from './child-element/child-element.component';
+import { CustomDirective } from './CustomDirectives/CustomDirective';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildElementComponent
+    ChildElementComponent,
+    CustomDirective
   ],
   imports: [
     BrowserModule,
