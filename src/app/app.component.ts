4c836c9aea60864e0890682d4e26d73d1e821bea
Mohamed Elhussein <mohamed.elhussein@arpuplus.com>
2021-10-06 02:04:42 +0200

    trackBy to improve performance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent 
 {
   // remember ! is for strict class checking
+  employees: any[] ;
   numbers: number[];
+  numbers2: number[];
   
   constructor()
   {
+    this.employees = [{name: 'mohamed' , age : 15} , {name: 'ali' , age : 25} , {name: 'omar' , age : 20}]
     this.numbers = [1,2,3,4, 6] ;
+    this.numbers2 = [7,8,9,10, 11] ;
   }
 
+  refreshAll()
+  {
+    this.employees = [{name: 'mohamed' , age : 15} , {name: 'ali' , age : 25} , {name: 'omar' , age : 20} , {name: 'Samy' , age : 30}];
+    this.numbers2 = [7,8,9,10, 11] ;
+  }
+  
+  trackEmployees (index: number , element: {name: string , age: number})
+  {
+    console.log('track index is called') ;
 
+    console.log('number is:' + element.name + '----' + 'index is ' + index );
+  }
   
 
 }
