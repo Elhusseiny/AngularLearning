bc58b522f54df5af92ceb06a39a312cbbd313db9
Mohamed Elhussein <mohamed.elhussein@arpuplus.com>
2021-10-06 04:02:16 +0200

    custom directive 1

---
+++ src/app/CustomDirectives/CustomDirective.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[styleMe]'
+})
+export class CustomDirective implements OnInit {
+  constructor(private elementRef: ElementRef) {
+  }
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
+}
