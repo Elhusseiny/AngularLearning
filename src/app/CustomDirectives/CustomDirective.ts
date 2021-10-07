22426915d9d484bd64411f383fc6cb30b2992682
Mohamed Elhussein <mohamed.elhussein@arpuplus.com>
2021-10-07 14:45:55 +0200

    making the directive properties bindable from the outside using @input properties.

--- src/app/CustomDirectives/CustomDirective.ts
+++ src/app/CustomDirectives/CustomDirective.ts
@@ -1,15 +1,18 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[styleMe]'
 })
 export class CustomDirective implements OnInit {
 
-  @HostBinding('style.backgroundColor') myChosenBackGroundColor : string = 'transparent';
+  @Input('highlightColor') highlightColor! : string  ;
+  @Input('basicColor') basicColor! : string  ;
+
+  @HostBinding('style.backgroundColor') myChosenBackGroundColor : string = this.basicColor;
 
    constructor(private elementRef : ElementRef , private renderer: Renderer2)
   {
-
+    this.myChosenBackGroundColor = 'yellow';
   }
   
   ngOnInit() {
@@ -20,11 +23,12 @@ export class CustomDirective implements OnInit {
   @HostListener('mouseover') mouseover(eventData: Event) //@HostListener('enter the event name')
   {
     //this.renderer.setStyle(this.elementRef.nativeElement , 'background-color' , 'red');
-    this.myChosenBackGroundColor = 'red' ;
+    //this.myChosenBackGroundColor = 'red' ;
+    this.myChosenBackGroundColor = this.highlightColor ;
   } 
   @HostListener('mouseleave') mouseleave(eventData: Event) //@HostListener('enter the event name')
   {
-    this.myChosenBackGroundColor = 'transparent' ;
+    this.myChosenBackGroundColor = this.basicColor ;
 
   } 
   
