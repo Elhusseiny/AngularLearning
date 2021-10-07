2cac1cc04f66adad130c102d4121b4a58052c5a7
Mohamed Elhussein <mohamed.elhussein@arpuplus.com>
2021-10-07 13:53:05 +0200

    HostListener1

--- src/app/CustomDirectives/CustomDirective.ts
+++ src/app/CustomDirectives/CustomDirective.ts
@@ -1,14 +1,28 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[styleMe]'
 })
 export class CustomDirective implements OnInit {
-  constructor(private elementRef: ElementRef , private renderer :  Renderer2 ) {
-  }
 
+
+   constructor(private elementRef : ElementRef , private renderer: Renderer2)
+  {
+
+  }
+  
   ngOnInit() {
     //this.elementRef.nativeElement.style.backgroundColor = 'green';
     this.renderer.setStyle(this.elementRef.nativeElement , 'background-color' , 'green');
   }
+
+  @HostListener('mouseover') mouseover(eventData: Event) //@HostListener('enter the event name')
+  {
+    this.renderer.setStyle(this.elementRef.nativeElement , 'background-color' , 'red');
+  } 
+  @HostListener('mouseleave') mouseleave(eventData: Event) //@HostListener('enter the event name')
+  {
+    this.renderer.setStyle(this.elementRef.nativeElement , 'background-color' , 'green');
+  } 
+  
 }
